5696f81cff6280c7680320fe411f7c3be5be3ef0
Lambert Galit <lbg.globagility@gmail.com>
2017-12-07 14:43:50 +0800

    made report to print employee id changes history

---
+++ gotescopayrolldb/RPT_employeeidhistory.sql
@@ -0,0 +1,29 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+DROP PROCEDURE IF EXISTS `RPT_employeeidhistory`;
+DELIMITER //
+CREATE DEFINER=`root`@`localhost` PROCEDURE `RPT_employeeidhistory`(IN `og_rowid` INT)
+BEGIN
+
+SELECT i.EmployeeRowID `DatCol1`
+, (@mid_init := LEFT(i.MiddleName, 1)) `MiddleInitial`
+, CONCAT_WS(', ', i.LastName, i.FirstName, IF(LENGTH(@mid_init) = 0, NULL, CONCAT(@mid_init, '.'))) `DatCol2`
+, i.NewValue `DatCol3`
+, i.ActionPerformed `DatCol4`
+, '' `DatCol50`
+FROM employeeidhistory i
+WHERE i.OrganizationID = og_rowid
+ORDER BY CONCAT(i.LastName, i.FirstName, i.MiddleName)
+         , i.Created
+;
+
+END//
+DELIMITER ;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ gotescopayrolldb/employeeidhistory.sql
@@ -0,0 +1,36 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+DROP VIEW IF EXISTS `employeeidhistory`;
+DROP TABLE IF EXISTS `employeeidhistory`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` VIEW `employeeidhistory` AS SELECT 
+aut.RowID
+, e.RowID `EmployeeRowID`
+, v.OrganizationID
+, e.EmployeeID
+, e.LastName, e.FirstName, e.MiddleName
+, pos.PositionName
+, aut.OldValue
+, aut.NewValue
+, aut.ActionPerformed
+, aut.Created
+FROM audittrail aut
+INNER JOIN `view` v
+        ON v.ViewName = 'Employee Personal Profile'
+		     # AND v.OrganizationID = 3
+		     AND aut.ViewID = v.RowID
+INNER JOIN employee e
+        ON e.RowID=aut.ChangedRowID
+		     AND e.OrganizationID=v.OrganizationID
+		     AND e.EmploymentStatus NOT IN ('Resigned', 'Terminated')
+INNER JOIN `position` pos
+        ON pos.RowID=e.PositionID
+WHERE aut.FieldChanged = 'EmployeeID'
+AND aut.ActionPerformed IN ('Insert', 'Update') ;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

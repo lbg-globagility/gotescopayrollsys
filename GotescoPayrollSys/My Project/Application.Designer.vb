c75a1a1be7b793e59c1c68106fda2920c914a0e4
Lambert <lbg.globagility@gmail.com>
2018-04-02 09:35:17 +0800

    some code changes migrating from my laptop to GLOBAL-D-PC

--- GotescoPayrollSys/My Project/Application.Designer.vb
+++ GotescoPayrollSys/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.GotescoPayrollSys.TimeEntryLogs
+            Me.MainForm = Global.GotescoPayrollSys.MetroLogin
         End Sub
     End Class
 End Namespace

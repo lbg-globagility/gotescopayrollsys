83c2a1193fac01f9b6c61cac0d467544e9b3ddd1
Lambert <lbg.globagility@gmail.com>
2019-04-23 13:57:18 +0800

    track entities folder

---
+++ GotescoPayrollSys/Core Forms/user_predefines/MySQL/entities/PrivilegeType.vb
@@ -0,0 +1,15 @@
+﻿Option Strict On
+
+Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.ComponentModel.DataAnnotations
+
+<Table("view")>
+Public Class PrivilegeType
+    <Key>
+    <DatabaseGenerated(DatabaseGeneratedOption.Identity)>
+    Public Property RowID As Integer
+
+    Public Property ViewName As String
+    Public Property OrganizationID As Integer
+
+End Class
---
+++ GotescoPayrollSys/Core Forms/user_predefines/MySQL/entities/newphilhealthimplement.vb
@@ -0,0 +1,16 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
+
+<Table("newphilhealthimplement")>
+Public Class newphilhealthimplement
+    <Key>
+    Public Property RowID As Integer
+    Public Property DeductionType As String
+
+    Public Property Rate As Decimal
+
+    Public Property MinimumContribution As Decimal
+
+    Public Property MaximumContribution As Decimal
+
+End Class

90b4bd87e94b994f1861674bf3e344425d17f90e
Lambert Galit <lbg.globagility@gmail.com>
2019-07-12 09:07:16 +0800

    save changes made to the database objects

--- gotescopayrolldb/INSUPD_paystubitemallowances.sql
+++ gotescopayrolldb/INSUPD_paystubitemallowances.sql
@@ -11,6 +11,13 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `INSUPD_paystubitemallowances`(
 	IN `e_rowid` INT,
 	IN `pp_rowid` INT,
 	IN `user_rowid` INT
+
+
+
+
+
+
+
 )
     DETERMINISTIC
 BEGIN
@@ -104,33 +111,43 @@ SET @day_pay = 0.00;
 
 # ***************************************************** # ***************************************************** #
 
+DROP TEMPORARY TABLE IF EXISTS semimonthlyallowanceforthisperiod; DROP TABLE IF EXISTS semimonthlyallowanceforthisperiod;
+CREATE TEMPORARY TABLE semimonthlyallowanceforthisperiod
+SELECT i.ProductID
+,og_rowid `OrganizationID`
+,CURRENT_TIMESTAMP() `Created`
+,user_rowid `CreatedBy`
+,ps_rowid `PayStubID`
+,ROUND(i.`AllowanceAmount`, 2) `AllowanceAmount`
+,FALSE `Undeclared`
+FROM (SELECT i.ProductID
+      ,i.EmployeeID
+      # ,( i.AllowanceAmount - (SUM(i.HoursToLess) * ((i.AllowanceAmount / (i.WorkDaysPerYear / (i.PAYFREQDIV * 12))) / 8)) ) `AllowanceAmount`
+#			,i.AllowanceAmount - TRIM(SUM( IFNULL((i.HoursToLess * (NULLIF(i.DailyAllowance, 0) / default_min_hrswork)), 0) ))+0 `AllowanceAmount`
+		,i.AllowanceAmount - TRIM(SUM(i.HoursToLess * (i.DailyAllowance / default_min_hrswork)))+0 `AllowanceAmount`
+      FROM paystubitem_sum_semimon_allowance_group_prodid i
+		WHERE i.OrganizationID=og_rowid
+		AND i.EmployeeID=e_rowid
+		# AND i.TaxableFlag = FALSE
+		AND i.`Date` BETWEEN date_from AND date_to
+#		AND NULLIF(i.DailyAllowance, 0) IS NOT NULL
+		GROUP BY i.ProductID, i.TaxableFlag
+#		GROUP BY i.EmployeeID, i.ProductID, i.TaxableFlag
+		) i
+#WHERE i.EmployeeID=e_rowid
+;
+
 # INSERT LOW_PRIORITY INTO paystubitem(`ProductID`,`OrganizationID`,`Created`,`CreatedBy`,`PayStubID`,`PayAmount`,`Undeclared`)
 INSERT INTO paystubitem(`ProductID`,`OrganizationID`,`Created`,`CreatedBy`,`PayStubID`,`PayAmount`,`Undeclared`)
-	SELECT i.ProductID
-	,og_rowid
-	,CURRENT_TIMESTAMP()
-	,user_rowid
-	,ps_rowid
-	,ROUND(i.`AllowanceAmount`, 2)
-	,0
-	FROM (SELECT i.ProductID
-	      ,i.EmployeeID
-	      # ,( i.AllowanceAmount - (SUM(i.HoursToLess) * ((i.AllowanceAmount / (i.WorkDaysPerYear / (i.PAYFREQDIV * 12))) / 8)) ) `AllowanceAmount`
-			,i.AllowanceAmount - TRIM(SUM(i.HoursToLess * (i.DailyAllowance / default_min_hrswork)))+0 `AllowanceAmount`
-	      FROM paystubitem_sum_semimon_allowance_group_prodid i
-			WHERE i.OrganizationID=og_rowid
-			AND i.EmployeeID=e_rowid
-			# AND i.TaxableFlag = FALSE
-			AND i.`Date` BETWEEN date_from AND date_to
-			# GROUP BY i.EmployeeID,ii.RowID,ii.ProductID
-			GROUP BY i.ProductID, i.TaxableFlag) i
+SELECT * FROM semimonthlyallowanceforthisperiod i
 ON
 DUPLICATE
 KEY
 UPDATE
 	LastUpd=CURRENT_TIMESTAMP()
 	,LastUpdBy=user_rowid
-	,PayAmount=ROUND(i.`AllowanceAmount`, 2);
+	,PayAmount=ROUND(i.`AllowanceAmount`, 2)
+	;
 
 END//
 DELIMITER ;

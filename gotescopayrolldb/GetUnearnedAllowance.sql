5af7145b1821f09635f5305fdd05c4be81c5d663
Lambert Galit <lbg.globagility@gmail.com>
2019-07-26 12:00:23 +0800

    correction in some data type calculation

--- gotescopayrolldb/GetUnearnedAllowance.sql
+++ gotescopayrolldb/GetUnearnedAllowance.sql
@@ -28,18 +28,15 @@ SET @hourlyAllowance=0.00;
 SET @monthCount=12;
 SET @twiceAMonth=2;
 
-DROP TEMPORARY TABLE IF EXISTS unearnedallowance; DROP TABLE IF EXISTS unearnedallowance; CREATE TEMPORARY TABLE unearnedallowance
+DROP TEMPORARY TABLE IF EXISTS unearnedallowance0; DROP TABLE IF EXISTS unearnedallowance0; CREATE TEMPORARY TABLE unearnedallowance0
 SELECT
 i.EmployeeID
-/* , (SUM(i.LateHours) * TRIM(i.TotalAllowanceAmt / 8)+0) `LateAllowance`
-, (SUM(i.UndertimeHours) * TRIM(i.TotalAllowanceAmt / 8)+0) `UndertimeAllowance`
-, (SUM(i.AbsentHours) * TRIM(i.TotalAllowanceAmt / 8)+0) `AbsentAllowance`
-*/
 , @dailyAllowance := (i.AllowanceAmount / ((e.WorkDaysPerYear / @monthCount) / @twiceAMonth)) `DailyAllowance`
-, @hourlyAllowance := @dailyAllowance / 8 `HourlyAllowance`
-, TRIM(SUM(i.LateHours * @hourlyAllowance))+0 `LateAllowance`
-, TRIM(SUM(i.UndertimeHours * @hourlyAllowance))+0 `UndertimeAllowance`
-, TRIM(SUM(i.AbsentHours * @hourlyAllowance))+0 `AbsentAllowance`
+, @hourlyAllowance := TRIM(@dailyAllowance / 8)+0 `HourlyAllowance`
+
+, TRIM(SUM(i.LateHours))+0 `SumLateHours`
+, TRIM(SUM(i.UndertimeHours))+0 `SumUndertimeHours`
+, TRIM(SUM(i.AbsentHours))+0 `SumAbsentHours`
 
 FROM paystubitem_sum_semimon_allowance_group_prodid i
 INNER JOIN employee e ON e.RowID=i.EmployeeID
@@ -48,6 +45,15 @@ AND i.`Date` BETWEEN @dateFrom AND @dateTo
 GROUP BY i.EmployeeID
 #HAVING SUM(i.LateHours + i.UndertimeHours + i.AbsentHours) > 0
 HAVING SUM(i.HoursToLess) > 0
+;
+
+DROP TEMPORARY TABLE IF EXISTS unearnedallowance; DROP TABLE IF EXISTS unearnedallowance; CREATE TEMPORARY TABLE unearnedallowance
+SELECT *
+, TRIM(i.HourlyAllowance * i.SumLateHours)+0 `LateAllowance`
+, TRIM(i.HourlyAllowance * i.SumUndertimeHours)+0 `UndertimeAllowance`
+, TRIM(i.HourlyAllowance * i.SumAbsentHours)+0 `AbsentAllowance`
+
+FROM unearnedallowance0 i
 ;
 
 END//

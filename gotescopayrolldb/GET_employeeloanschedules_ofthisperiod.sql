11914cbd43c79e93c8ddf888f0ab9417bbb1fd61
Lambert G <lbg.globagility@gmail.com>
2022-06-08 17:19:23 +0800

    fix: loan schedule strict based on current pay period schedule

--- gotescopayrolldb/GET_employeeloanschedules_ofthisperiod.sql
+++ gotescopayrolldb/GET_employeeloanschedules_ofthisperiod.sql
@@ -16,12 +16,15 @@ CREATE PROCEDURE `GET_employeeloanschedules_ofthisperiod`(
 BEGIN
 
 DECLARE datefrom, dateto DATE;
+DECLARE isEnd INT;
 
 SELECT
-pp.*
+pp.PayFromDate,
+pp.PayToDate,
+pp.Half
 FROM payperiod pp
-WHERE pp.RowID
-INTO datefrom, dateto;
+WHERE pp.RowID=payperiod_rowid
+INTO datefrom, dateto, isEnd;
 
 CALL `LoanPrediction`(org_rowid);
 
@@ -34,12 +37,14 @@ FROM (SELECT i.*
 		WHERE i.PayperiodID=payperiod_rowid
 		AND LCASE(i.`Status`) IN ('in progress', 'complete')
 		AND i.DiscontinuedDate IS NULL
+		AND IF(isEnd=0, i.DeductionSchedule IN ('End of the month', 'Per pay period'), i.DeductionSchedule IN ('First half', 'Per pay period'))
 	UNION
 		SELECT i.*
 		FROM loanpredict i
 		WHERE i.PayperiodID=payperiod_rowid
 		AND LCASE(i.`Status`) = 'cancelled'
 		AND i.DiscontinuedDate IS NOT NULL
+		AND IF(isEnd=0, i.DeductionSchedule IN ('End of the month', 'Per pay period'), i.DeductionSchedule IN ('First half', 'Per pay period'))
 		AND ((datefrom BETWEEN i.DedEffectiveDateFrom AND i.SubstituteEndDate) OR
 				(dateto BETWEEN i.DedEffectiveDateFrom AND i.SubstituteEndDate))
 		) ii

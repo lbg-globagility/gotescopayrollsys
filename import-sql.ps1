c5778230448b3202bd958cc4a5d7f1ce39ad2a2e
Lambert Galit <lbg.globagility@gmail.com>
2023-08-03 14:37:37 +0800

    fix: allow user to specify port value

--- import-sql.ps1
+++ import-sql.ps1
@@ -1,5 +1,6 @@
 $server = Read-Host 'server';
 $database = Read-Host 'database';
+$port = Read-Host 'port';
 $pwd = Read-Host 'password' -AsSecureString;
 
 $pwd = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pwd));
@@ -10,11 +11,11 @@ Foreach-Object {
 
     If (!$server) {
         Get-Content $filename |
-            mysql --user=root --password=$pwd --database=$database --comments;
+            mysql --user=root --password=$pwd --database=$database --port=$port --comments;
     }
     Else {
         Get-Content $filename |
-            mysql --user=root --host=$server --password=$pwd --database=$database --comments;
+            mysql --user=root --host=$server --password=$pwd --database=$database --port=$port --comments;
     }
 
     Write-Output $filename;
